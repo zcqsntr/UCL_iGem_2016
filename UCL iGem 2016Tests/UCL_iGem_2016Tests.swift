6330c9108b114462be91d11873866084be988638
Neythen Treloar <neythen.t@gmail.com>
2016-07-01 15:18:08 +0100

    Initial Commit

---
+++ UCL iGem 2016Tests/UCL_iGem_2016Tests.swift
@@ -0,0 +1,36 @@
+//
+//  UCL_iGem_2016Tests.swift
+//  UCL iGem 2016Tests
+//
+//  Created by Neythen Treloar on 01/07/2016.
+//  Copyright © 2016 Neythen Treloar. All rights reserved.
+//
+
+import XCTest
+@testable import UCL_iGem_2016
+
+class UCL_iGem_2016Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
